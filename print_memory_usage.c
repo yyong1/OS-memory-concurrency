c636c90aa15bf587187e1e54f9945f5647186aa6
Andrey Baranov <baranov.a.64103@gmail.com>
2023-05-26 23:04:24 +0200

    fix functin

--- print_memory_usage.c
+++ print_memory_usage.c
@@ -2,28 +2,19 @@
 #include <stdlib.h>
 #include <sys/mman.h>
 #include <unistd.h>
+#include <string.h>
 
 #define PAGE_SIZE 4096
 
 void print_memory_usage() {
-    // Read and print memory usage from /proc/<pid>/status file
-    char stat_file_path[256];
-    sprintf(stat_file_path, "/proc/%d/status", getpid());
-
-    FILE* stat_file = fopen(stat_file_path, "r");
-    if (stat_file == NULL) {
-        perror("Failed to open stat file");
+    struct rusage usage;
+    if (getrusage(RUSAGE_SELF, &usage) != 0) {
+        perror("Failed to get resource usage");
         return;
     }
 
-    char line[256];
-    while (fgets(line, sizeof(line), stat_file) != NULL) {
-        if (strstr(line, "VmSize") || strstr(line, "VmRSS")) {
-            printf("%s", line);
-        }
-    }
-
-    fclose(stat_file);
+    printf("Memory usage:\n");
+    printf("Virtual Memory Size (VSZ): %ld KB\n", usage.ru_maxrss);
 }
 
 int main() {
